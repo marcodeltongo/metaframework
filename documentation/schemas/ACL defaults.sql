158a4ce693041a6043bf495db45416d6bfe4f0df
Marco Del Tongo <info@marcodeltongo.com>
2011-07-05 19:00:44 +0200

    Updated ACL Schema & defaults.

--- documentation/schemas/ACL defaults.sql
+++ documentation/schemas/ACL defaults.sql
@@ -1,8 +1,16 @@
 /*
 -- Query:
--- Date: 2011-06-23 16:08
+-- Date: 2011-07-05 15:51
 */
-INSERT INTO `AuthItem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES ('guest',2,'Guest user role','return Yii::app()->user->getIsGuest();',NULL);
-INSERT INTO `AuthItem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES ('authenticated',2,'Authenticated user role','return !Yii::app()->user->getIsGuest();',NULL);
-INSERT INTO `AuthItem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES ('administrator',2,'Administrator user role',NULL,NULL);
-INSERT INTO `AuthItem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES ('*',1,'SuperPowers Task',NULL,NULL);
+
+INSERT INTO `AuthItem` (`name`,`type`,`description`,`bizrule`,`data`) VALUES
+('guest',2,'Guest user role','return Yii::app()->user->getIsGuest();',NULL),
+('authenticated',2,'Authenticated user role','return !Yii::app()->user->getIsGuest();',NULL),
+('administrator',2,'Administrator user role',NULL,NULL),
+('*.site',1,'SiteController all actions',NULL,NULL),
+('index.site',0,'SiteController::actionIndex',NULL,NULL),
+('*.user',1,'UserController all actions',NULL,NULL),
+('*',1,'SuperPowers Task',NULL,NULL);
+
+INSERT INTO `AuthItemChild` VALUES
+('administrator', '*');
