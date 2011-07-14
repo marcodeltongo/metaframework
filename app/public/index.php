b2754091c35f4fb9f5a5dd6013b07a83e14da0be
Marco Del Tongo <info@marcodeltongo.com>
2011-07-14 19:05:59 +0200

    Directory separator fixes for Windows

--- app/public/index.php
+++ app/public/index.php
@@ -11,17 +11,22 @@
  */
 
 /*
+ * Shortcut constants.
+ */
+define('DS', DIRECTORY_SEPARATOR);
+
+/*
  * Prepares constants for paths and URLs.
  */
-defined('PUBLIC_DIR') or define('PUBLIC_DIR', realpath(dirname(__FILE__)) . '/');
-defined('ROOT_DIR') or define('ROOT_DIR', realpath(PUBLIC_DIR . '../..') . '/');
-defined('COMMON_DIR') or define('COMMON_DIR', ROOT_DIR . 'common/');
-defined('VENDORS_DIR') or define('VENDORS_DIR', COMMON_DIR . 'vendors/');
-defined('YII_DIR') or define('YII_DIR', VENDORS_DIR . 'Yii/');
-defined('ZEND_DIR') or define('ZEND_DIR', VENDORS_DIR . 'Zend/');
-defined('PHPEXCEL_DIR') or define('PHPEXCEL_DIR', VENDORS_DIR . 'PHPExcel/');
-defined('PHPMAILER_DIR') or define('PHPMAILER_DIR', VENDORS_DIR . 'PHPMailer/');
-defined('APP_DIR') or define('APP_DIR', realpath(PUBLIC_DIR . '..') . '/');
+defined('PUBLIC_DIR') or define('PUBLIC_DIR', realpath(dirname(__FILE__)) . DS);
+defined('ROOT_DIR') or define('ROOT_DIR', realpath(PUBLIC_DIR . '../..') . DS);
+defined('COMMON_DIR') or define('COMMON_DIR', ROOT_DIR . 'common' . DS);
+defined('VENDORS_DIR') or define('VENDORS_DIR', COMMON_DIR . 'vendors' . DS);
+defined('YII_DIR') or define('YII_DIR', VENDORS_DIR . 'Yii' . DS);
+defined('ZEND_DIR') or define('ZEND_DIR', VENDORS_DIR . 'Zend' . DS);
+defined('PHPEXCEL_DIR') or define('PHPEXCEL_DIR', VENDORS_DIR . 'PHPExcel' . DS);
+defined('PHPMAILER_DIR') or define('PHPMAILER_DIR', VENDORS_DIR . 'PHPMailer' . DS);
+defined('APP_DIR') or define('APP_DIR', realpath(PUBLIC_DIR . '..') . DS);
 
 set_include_path(VENDORS_DIR . PATH_SEPARATOR . ROOT_DIR . PATH_SEPARATOR . get_include_path());
 
@@ -70,7 +75,7 @@ spl_autoload_register(array('YiiBase', 'autoload'));
 // define('APPSUITE_ID', 'your_own_unique_app_name');
 // /*
 if (!defined('APPSUITE_ID')) {
-    $_parts = explode(DIRECTORY_SEPARATOR, APP_DIR);
+    $_parts = explode(DS, APP_DIR);
     define('APPSUITE_ID', $_parts[count($_parts) - 3]);
 }
 // */
@@ -81,7 +86,7 @@ if (!defined('APPSUITE_ID')) {
 // define('APP_ID', 'your_own_unique_app_name');
 // /*
 if (!defined('APP_ID')) {
-    $_parts = explode(DIRECTORY_SEPARATOR, APP_DIR);
+    $_parts = explode(DS, APP_DIR);
     define('APP_ID', $_parts[count($_parts) - 2]);
 }
 // */
@@ -99,11 +104,11 @@ $config = CMap::mergeArray($commonConfig, $appConfig);
 Yii::setPathOfAlias('RootAlias', ROOT_DIR);
 Yii::setPathOfAlias('common', COMMON_DIR);
 Yii::setPathOfAlias('system', YII_DIR);
-Yii::setPathOfAlias('zii', YII_DIR . DIRECTORY_SEPARATOR . 'zii');
+Yii::setPathOfAlias('zii', YII_DIR . DS . 'zii');
 Yii::setPathOfAlias('application', APP_DIR);
 Yii::setPathOfAlias('webroot', PUBLIC_DIR);
 Yii::setPathOfAlias('app', APP_DIR);
-Yii::setPathOfAlias('ext', COMMON_DIR . DIRECTORY_SEPARATOR . 'extensions');
+Yii::setPathOfAlias('ext', COMMON_DIR . DS . 'extensions');
 
 /*
  * Some helpers.
